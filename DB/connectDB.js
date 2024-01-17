abb19560e5558266acc46846c019292e26e55e46
= <=>
2024-01-17 10:50:41 +0500

    connect to MongoDB

---
+++ DB/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default connectDB;
